547afe41b3948c5e70d6288612bb9696ff6dfeda
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2023-03-16 19:56:57 +0100

    Recipe.js file added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
 
-  /* const APP_ID = '56110c6e';
-  const APP_KEY = 'ae9bd981dbe1ff09ea106fc8acd90fe1'; */
-  const reqUrl = "https://api.edamam.com/api/recipes/v2?type=public&app_id=56110c6e&app_key=ae9bd981dbe1ff09ea106fc8acd90fe1"
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch("https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=56110c6e&app_key=ae9bd981dbe1ff09ea106fc8acd90fe1");
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
 
   return (
     <div className='App'>
-      <form>
-        <input type="text" />
-        <button type='submit'>Search</button>
+      <form className='search-form'>
+        <input className='search-bar' type="text" />
+        <button className='search-button' type='submit'>Search</button>
       </form>
     </div>
   )
